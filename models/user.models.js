ebd1f9038136e8ac63c49c82dd6772a62c9be6b2
Divyansh <officialdslc1552005@gmail.com>
2026-01-06 12:49:32 +0530

    Access and refresh Token learned, mailgen setup done

--- models/user.models.js
+++ models/user.models.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt"
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { createHmac } from "crypto";
 
 const userSchema = new mongoose.Schema(
   {
@@ -63,17 +64,60 @@ const userSchema = new mongoose.Schema(
 );
 
 // save operation kroge tab hash krdena password ie har save operation par ye chalega chaahe image save karo ya aur kuch
-// iss baar baar hashing se bachne k liye we use if isModified ie pre hook tabhi run ho jab 
-userSchema.pre("save", async function(next){
-  if(!this.isModified("password")) return next()
-  this.password = await bcrypt.hash(this.passwords, 10)
-  next()
-})
+// iss baar baar hashing se bachne k liye we use if isModified ie pre hook tabhi run ho jab
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.passwords, 10);
+  next();
+});
 
-// method to verify password 
-userSchema.methods.isPasswordCorrect = async function(password){
+// method to verify password
+userSchema.methods.isPasswordCorrect = async function (password) {
   // will return a true or false based on comparison
-  return await bcrypt.compare(password, this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
+
+// tokens
+userSchema.methods.generateAccessToken = function () {
+  return (
+    jwt.sign(
+      {
+        _id: this._id, // for payload
+        email: this.email,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+    ),
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+// make sure tokens have small payload since unko har request mein send krna hota hai, its a good practice to keep them small
+
+// refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return (
+    jwt.sign(
+      {
+        _id: this._id, // for payload
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+    ),
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+// crypto token for reset password, user verfication
+userSchema.methods.generateTemporaryToken = function () {
+  const rawToken = crypto.randomlyBytes(20).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(rawToken)
+    .digest("hex");
+
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20 minutes
+
+  return { rawToken, hashedToken, tokenExpiry };
+};
 
 export const userModel = mongoose.model("user", userSchema);
