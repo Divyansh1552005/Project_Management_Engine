9b081b6e9e942a9068f778891f5076a800976cb4
Divyansh <officialdslc1552005@gmail.com>
2026-01-08 12:41:30 +0530

    Completed Auth Controllers

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,17 @@
 import express, { Router } from "express";
-import { registerUser, login } from "../controllers/auth.controllers.js";
+import { registerUser, login, logoutUser } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { userRegisterValidator, userLoginValidator } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const userRouter = Router();
 
 userRouter.route("/register").post(userRegisterValidator(), validate,  registerUser);
 userRouter.route("/login").post(userLoginValidator(), validate, login);
+
+// secure routes
+userRouter.route("/logout").post(verifyJWT, logoutUser);
+
 
 
 export default userRouter;
